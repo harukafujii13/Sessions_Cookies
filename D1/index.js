099bb07b156fbbc132ee1ea8be81858f85b38733
Haruka <hfujii1013@gmail.com>
2023-05-31 20:36:48 -0700

    first

---
+++ D1/index.js
@@ -0,0 +1,12 @@
+const { application } = require("express");
+const session = require("express-session");
+
+const oneDay = 1000 * 60 * 60 * 24;
+application.use(
+  session({
+    secret: "thisismysecretekey",
+    saveUninitialized: true,
+    cookie: { maxAge: oneDay },
+    resave: false,
+  })
+);
